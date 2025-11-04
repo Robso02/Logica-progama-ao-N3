4d0f9466ef5116f49a28d3d2ec1bd09826382040
ROBSON DE SOUZA SANTOS <robson.ssantos26@senacsp.edu.br>
2025-11-03 21:30:30 -0300

    primeiro

---
+++ Aula 1/Aula 1/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string nome;
+//declaraçao de uma variavel
+nome = "rob garcia";
+
+//atribuiçao de uma variavel
+Console.WriteLine(nome);
+
+nome = "rob";
+
+Console.WriteLine(nome);
+
+// declaraçao + atribuiçao de uma constante
+const string constante = "minha constante";
+
+Console.WriteLine(constante);
+
+int idade = 19;
+float altura = 1.8f;
+
+int x = 1;
+int y = 2;
+int z = 3;
+
+double resultado = (x + y) * z / + 3.0;
+
+resultado += 100;
+
+Console.WriteLine ("hello, World!");
+Console.WriteLine("ola, mundo!");
+Console.WriteLine(resultado < 100);
+
+Console.WriteLine("/n-----/n");
+
+string nomev= "Rob";
+
+string sobrenome = "sousa";
+Console.WriteLine(nome + sobrenome);
+
+char LetraA = '0';
+char Letra = '1';
+Console.WriteLine(LetraA + 0);
+Console.WriteLine(Letra + 0);
+
+Console.WriteLine("Escreva seu nome.");
+string nomeUsusarip = Console.ReadLine();
+Console.WriteLine("o seu nome e:" + nomeUsusarip);
+
+Console.WriteLine("Qual e sua idade");
+String idadeUsuario = Console.ReadLine();
+ int.Parse("IDADEUSUARIO");
+
+Console.WriteLine("a sua idade e: " );
+
+    //1 - Peça´pa5ra o usuario inserir dois mumeros inteiros mostreos-os no console
+
+  Console.WriteLine("escreva dois numero inteiro");
+String numero1 = Console.ReadLine();
+Console.WriteLine("escreva seu segundo numero:");
+String numero2 = Console.ReadLine();
+int num1 = int.Parse("numero1");
+int num2 = int.Parse("numero2");
+int num3 = int.Parse("numero3");
+int nu4 = int.Parse("NUMERO 4");
